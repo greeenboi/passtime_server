7ae4c276137d015d15176b1631109a149779b7a1
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-05-07 01:35:42 +0530

    first commit

---
+++ api/index.ts
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
